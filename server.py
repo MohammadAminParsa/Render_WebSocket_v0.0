b50a56a2a616a216e9c72da62080119a367bbbd3
MohammadAminParsa <39618063+MohammadAminParsa@users.noreply.github.com>
2025-06-12 17:15:02 +0330

    Update server.py

--- server.py
+++ server.py
@@ -3,24 +3,24 @@ from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # فعال‌سازی CORS برای اتصال ESP32
+CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# داده‌ی ولتاژ از ESP32 دریافت و به مرورگر broadcast می‌شود
+# داده‌ی ولتاژ از ESP32
 @socketio.on('esp_data')
 def handle_esp_data(data):
-    print("📥 ولتاژ دریافتی از ESP32:", data)
+    print("📥 دریافت از ESP32:", data)
     emit('update_voltage', data, broadcast=True)
 
-# دستور روشن/خاموش از مرورگر گرفته و برای ESP32 ارسال می‌شود
+# دستور کنترل از مرورگر به ESP32
 @socketio.on('switch_command')
 def handle_switch_command(data):
-    print("📤 ارسال دستور به ESP32:", data)
+    print("📤 فرمان به ESP32:", data)
     emit('control_command', data, broadcast=True)
 
 @app.route('/')
 def index():
-    return "سرور WebSocket برای مانیتورینگ اینورتر آماده است."
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=10000)
