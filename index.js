59af11cdfbda17729876ccbe25d68cd2c40ff88a
MohammadAminParsa <39618063+MohammadAminParsa@users.noreply.github.com>
2025-06-28 09:32:46 +0330

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 const app = express();
 
 // پورت ۵۰۰۰ پیش‌فرض Render است
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server, path: "/ws" });
