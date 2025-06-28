82990e8aa55b72e859b2ea652aeba7a16776291e
MohammadAminParsa <39618063+MohammadAminParsa@users.noreply.github.com>
2025-06-28 21:04:00 +0330

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 # data.py
 voltage = "0.000"
 
-def set_voltage(v):
-    global voltage
-    voltage = v
-
 def get_voltage():
     return voltage
+
+def set_voltage(v: str):
+    global voltage
+    voltage = v
